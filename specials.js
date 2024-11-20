658c097715f48a29ac195e2c2583dd9c5135de95
Carlos Suarez <ingcarlos.suarez.moreno@gmail.com>
2024-11-20 15:45:54 -0700

    commented out not required code

--- specials.js
+++ specials.js
@@ -1,20 +1,20 @@
-const JSON = [   
-    {
-        "day": "Wednesday",
-        "name": "$12 BBQ Wings",
-        "description": "With celeries and carrots"
-    },
-    {
-        "day": "Friday",
-        "name": "$16 Ribbs",
-        "description": "BBQ and Spicy"
-    },
-    {
-        "day": "Thursday",
-        "name": "$12 Fish 'n ships",
-        "description": "Mayo cheese"
-    },
-];
+// const JSON = [   
+//     {
+//         "day": "Wednesday",
+//         "name": "$12 BBQ Wings",
+//         "description": "With celeries and carrots"
+//     },
+//     {
+//         "day": "Friday",
+//         "name": "$16 Ribbs",
+//         "description": "BBQ and Spicy"
+//     },
+//     {
+//         "day": "Thursday",
+//         "name": "$12 Fish 'n ships",
+//         "description": "Mayo cheese"
+//     },
+// ];
 
 
 const currentDate = new Date();
